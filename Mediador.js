037dc701d11b93c17166a468a0ddf1eafd42dcae
Wilson <wjuma19@gmail.com>
2025-04-03 08:37:15 -0400

    agregado Mediador.js

---
+++ Mediador.js
@@ -0,0 +1,58 @@
+function Vendedor(nombre) {
+  this.nombre = nombre;
+  this,sala = null;
+}
+
+Vendedor.prototype = {
+  oferta(articulo, precio) {
+    console.log(`Tenemos el siguiente articulo ${articulo}, iniciamos con un precio de ${precio}`)
+  },
+  vendido(comprador) {
+    console.log(`Vendido a ${comprador.nombre}`)
+  }
+}
+
+function Comprador(nombre) {
+  this.nombre = nombre;
+  this,sala = null;
+}
+
+Comprador.prototype = {
+  oferta(cantidad, comprador) {
+    console.log( `${comprador.nombre}: ${cantidad}` )
+  }
+}
+
+function Subasta() {
+  let compradores = {};
+
+  return {
+    registrar(usuario) {
+      compradores[usuario.nombre] = usuario;
+      usuario.sala = this;
+    }
+  }
+}
+
+
+
+const juan = new Vendedor('juan');
+const pablo = new Comprador('pablo');
+const maria = new Comprador('maria');
+const subasta = new Subasta()
+
+subasta.registrar(pablo)
+subasta.registrar(juan)
+subasta.registrar(maria)
+
+juan.oferta('Honda CRV', 1350000)
+
+maria.oferta(300, maria)
+pablo.oferta(600, pablo)
+
+juan.vendido(pablo)
+
+
+console.log(juan)
+console.log(pablo)
+console.log(maria)
